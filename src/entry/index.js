f61f8a9f4f1feb724430f3bd0d5b7d5ea0963c18
YangMr <759168442@qq.com>
2023-10-27 10:27:24 +0800

    实现模型加载

---
+++ src/entry/index.js
@@ -0,0 +1,102 @@
+// 引入threejs
+import * as THREE from "three"
+
+// 导入模型加载器方法
+import { loadManager } from "../model/loadManager"
+
+// 导入汽车类方法
+import { Car } from "../model/Car"
+
+// 引入轨道控制器
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+
+// 创建场景、摄像机、渲染器、轨道控制器、全局变量
+let scene, camera, renderer, controls
+
+// 获取渲染的dom节点
+const app = document.querySelector(".app")
+
+// 初始化场景与摄像机
+function init() {
+    // 创建场景
+    scene = new THREE.Scene();
+
+    // 创建摄像机
+    camera = new THREE.PerspectiveCamera(75, app.clientWidth / app.clientHeight, 0.1, 1000)
+
+    // 设置相机的位置
+    camera.position.z = 5
+
+    // 创建渲染器
+    renderer = new THREE.WebGLRenderer({
+        // 开启抗锯齿
+        antialias: true
+    })
+
+    // 告诉渲染器显示阴影贴图
+    renderer.shadowMap.enabled = true
+
+    // 设置渲染器画布
+    renderer.setSize(app.clientWidth, app.clientHeight)
+
+    // 添加到APP的DOM节点中
+    app.appendChild(renderer.domElement)
+
+    // 加载模型
+    loadManager("glb/Lamborghini.glb", (gltf) => {
+        // 获取到模型对象
+        const model = gltf.scene
+        // 将模型对象加载到场景中
+        const car = new Car(model, scene)
+        car.init()
+    })
+}
+
+
+// 初始化轨道控制器
+function createControls() {
+    // 创建轨道控制器
+    controls = new OrbitControls(camera, renderer.domElement)
+}
+
+// 初始化坐标轴
+function createHelper() {
+    // 创建坐标轴
+    const axesHelper = new THREE.AxesHelper(5)
+    // 将坐标轴添加到场景
+    scene.add(axesHelper)
+}
+
+// 场景适配
+function resizeRender() {
+    // 监听浏览器窗口的变化
+    window.addEventListener("resize", () => {
+        // 设置渲染器画布的大小, 让它随着浏览器的变化而变化
+        renderer.setSize(app.clientWidth, app.clientHeight)
+        // 设置摄像机的宽高比, 让它随着浏览器的变化而变化
+        camera.aspect = app.clientWidth / app.clientHeight
+        // 更新物体的锥体空间, 让它随着浏览器的变化而变化
+        camera.updateProjectionMatrix()
+    })
+}
+
+// 循环渲染
+function renderLoop() {
+    // 将场景与摄像机渲染到画布上
+    renderer.render(scene, camera)
+    // 手动更新场景
+    controls.update()
+    // 循环渲染
+    requestAnimationFrame(renderLoop)
+}
+
+// 开始方法
+function start() {
+    init()
+    createControls()
+    createHelper()
+    resizeRender()
+    renderLoop()
+}
+// 调用开始方法
+start()
